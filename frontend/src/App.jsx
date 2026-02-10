972e2126c86afb303422874ac102ff4c68db6d19
Tanakorn <tanakorn@Tanakorn.local>
2026-02-10 22:14:11 +0700

    setup react vite mui frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h4" sx={{ mt: 4 }}>
+        Expense Tracker
+      </Typography>
+      
+    </Container>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Expense.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Expenses() {
+  return (
+    <>
+      <Typography variant="h5">Expenses</Typography>
+    </>
+  );
+}
